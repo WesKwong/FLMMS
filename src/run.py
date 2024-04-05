2a0445d8f0e162e843722431aee57dccab4922d9
WesKwong <weskwong@outlook.com>
2024-04-05 15:25:20 +0800

    Refactor run function in run.py to include random seed setting

--- src/run.py
+++ src/run.py
@@ -9,17 +9,18 @@ from loguru import logger
 logger.remove()
 logger.add(sys.stdout, level=config.log_level)
 glob.set('logger', logger)
+# -------------------- set random seed ------------------- #
+from tools.ExptUtils import set_seed
+set_seed(config.random_seed)
 # ------------------------- run main ------------------------- #
 import os
 RESULTS_PATH = os.environ['RESULTS_PATH']
 glob.set('results_path', RESULTS_PATH)
 
-
 @logger.catch
 def run():
     from main import main
     main()
-
 
 if __name__ == "__main__":
     run()

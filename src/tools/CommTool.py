5d129264fd7701bd7b2a134a82738a26d4af4cb9
WesKwong <weskwong@outlook.com>
2024-04-06 12:51:03 +0800

    Fix logging statements in CommTool.py

--- src/tools/CommTool.py
+++ src/tools/CommTool.py
@@ -29,20 +29,16 @@ def send(data, dst: int, tag=0):
     serialized_data = pickle.dumps(data)
     data_tensor = torch.tensor(list(serialized_data), dtype=torch.uint8).to(torch.device('cpu'))
     data_size = torch.tensor(len(data_tensor), dtype=int).to(torch.device('cpu'))
-    logger.debug(f'dst={dst}, sending {tag}: {data}')
     dist.send(data_size, dst=dst, tag=tag)
     dist.send(data_tensor, dst=dst, tag=tag)
-    logger.debug(f'dst={dst}, sent {tag}: {data}')
 
 def recv(src: int, tag=0):
     data_size = torch.tensor(0, dtype=int).to(torch.device('cpu'))
-    logger.debug(f'src={src}, receiving {tag}: ...')
     dist.recv(data_size, src=src, tag=tag)
     data_tensor = torch.empty(size=(data_size.item(),), dtype=torch.uint8).to(torch.device('cpu'))
     dist.recv(data_tensor, src=src, tag=tag)
     serialized_data = bytes(data_tensor.tolist())
     data = pickle.loads(serialized_data)
-    logger.debug(f'src={src}, recv {tag}: {data}')
     return data
 
 def broadcast(data, dsts: list, tag=0):

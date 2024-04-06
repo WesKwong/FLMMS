47edfeef40e6d9e4f52aa0626b2aeace93aa84a7
WesKwong <weskwong@outlook.com>
2024-04-06 18:47:38 +0800

    Refactor launch.py to use global_config instead of config

--- src/launch.py
+++ src/launch.py
@@ -1,24 +1,25 @@
 import os
 from loguru import logger
 
-from configs.MainConfig import config
-from tools.ExptUtils import get_unique_results_path
+from configs import global_config
+from tools.expt_utils import get_unique_results_path
 
 NETWORK_NAME = "flmms_net"
 IMAGE_NAME = "weskwong/flmms:v1.0"
 MNT_PATH = "$(pwd)"
-RESULTS_PATH = get_unique_results_path(config.results_path, config.expt_name)
+RESULTS_PATH = get_unique_results_path(global_config.results_path,
+                                       global_config.expt_name)
 
-MASTER_ADDR = "flmms_client_1"
+MASTER_ADDR = "flmms_server"
 MASTER_PORT = "11451"
-WORLD_SIZE = config.num_client + 1
-DEVICE = config.device
+WORLD_SIZE = global_config.num_client + 1
+DEVICE = global_config.device
 
 
 def get_torchrun_cmd(id):
     nproc_per_node = f"--nproc_per_node=1"
     nnodes = f"--nnodes={WORLD_SIZE}"
-    node_rank = f"--node_rank={id-1 if id != 0 else WORLD_SIZE-1}"
+    node_rank = f"--node_rank={id}"
     master_addr = f"--master_addr={MASTER_ADDR}"
     master_port = f"--master_port={MASTER_PORT}"
     torchrun_cmd = f"torchrun {nproc_per_node} {nnodes} {node_rank} {master_addr} {master_port} run.py"
@@ -45,7 +46,7 @@ def run_docker(id):
         log_name = os.path.join(RESULTS_PATH, "server.log")
     else:
         log_name = os.path.join(RESULTS_PATH, f"client_{id}.log")
-    sh_cmd = f"CUDA_VISIBLE_DEVICES={config.cuda_device[id]} RESULTS_PATH={RESULTS_PATH} {torchrun_cmd} > {log_name} 2>&1"
+    sh_cmd = f"CUDA_VISIBLE_DEVICES={global_config.cuda_device[id]} RESULTS_PATH={RESULTS_PATH} {torchrun_cmd} > {log_name} 2>&1"
     sh_cmd = f"sh -c '{sh_cmd}'"
     # run docker
     docker_cmd = f"docker run --rm {docker_run_mode} {network} {mnt} {container_name} {gpu_cmd} {image} {sh_cmd}"
@@ -54,9 +55,9 @@ def run_docker(id):
 
 @logger.catch
 def launch():
-    for i in range(1, config.num_client+1):
-        run_docker(i)
     run_docker(0)
+    for i in range(1, global_config.num_client + 1):
+        run_docker(i)
 
 
 if __name__ == "__main__":

93bb73934b912a14398f2f898d434f8e99899131
WesKwong <weskwong@outlook.com>
2024-04-06 19:37:35 +0800

    Refactor get_nn function to use net_name instead of net in neuralnets/__init__.py

--- src/neuralnets/__init__.py
+++ src/neuralnets/__init__.py
@@ -1,12 +1,14 @@
 from .lenet5 import LeNet5
 
 def get_nn(hp):
-    net = hp["net"]
+    net_name = hp["net"]
     dataset = hp["dataset"]
-    if net == "LeNet5":
+    if net_name == "LeNet5":
         if dataset == "CIFAR10":
-            return LeNet5(10, 3)
+            net = LeNet5(10, 3)
         else:
             raise ValueError(f"Invalid dataset: {dataset}")
     else:
-        raise ValueError(f"Invalid net: {net}")
+        raise ValueError(f"Invalid net: {net_name}")
+
+    return net

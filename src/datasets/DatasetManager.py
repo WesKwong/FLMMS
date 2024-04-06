7d8fb8fd06a7b16d2a17132d7b3343f3c8b30d22
WesKwong <weskwong@outlook.com>
2024-04-06 11:23:10 +0800

    fix syntax error

--- src/datasets/DatasetManager.py
+++ src/datasets/DatasetManager.py
@@ -1,6 +1,7 @@
-def get_dataset(hp):
-    if hp["dataset"] == 'CIFAR10':
+def get_dataset(hp: dict):
+    dataset = hp["dataset"]
+    if dataset == 'CIFAR10':
         from datasets.CIFAR10 import get_dataset
         return get_dataset()
     else:
-        raise ValueError(f"Invalid dataset: {hp["dataset"]}")
+        raise ValueError(f"Invalid dataset: {dataset}")
